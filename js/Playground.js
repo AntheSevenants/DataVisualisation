54c1ba29996d433cd69fc22b4509ffe58332a39d
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-03-30 12:49:54 +0000

    Add playground script

---
+++ js/Playground.js
@@ -0,0 +1,8 @@
+let linearRunningPlot = new LinearRunningPlot("linearRunningPlot",
+											  240,
+											  [ { "time": 31 },
+											  	{ "time": 33 },
+											  	{ "time": 36 },
+											  	{ "time": 38 },
+											  	{ "time": 41 },
+											  	{ "time": 42 } ]);
