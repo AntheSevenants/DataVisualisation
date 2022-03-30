f956a93bef2252b02f86793620bfa75f538de161
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-03-30 14:38:16 +0000

    Add Toolbar class

---
+++ js/Toolbar.js
@@ -0,0 +1,15 @@
+class Toolbar {
+	constructor(targetElement) {
+		// Where is the toolbar located?
+		this.targetElement = targetElement;
+		this.buttons = {};
+	}
+
+	registerButton(name, content, className, onClickEvent) {
+		this.buttons[name] = this.targetElement.append("button")
+											   .attr("id", name)
+											   .text(content)
+											   .attr("class", className)
+											   .on("click", onClickEvent);
+	}
+}
