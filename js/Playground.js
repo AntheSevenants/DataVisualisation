4dae8e02e3e6253a583bb1c2f7af509aea707ad7
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-04-06 12:19:59 +0000

    Add results table to playground

--- js/Playground.js
+++ js/Playground.js
@@ -1,9 +1,13 @@
+let efforts = [ { "time": 31 },
+				{ "time": 33 },
+				{ "time": 36 },
+				{ "time": 38 },
+				{ "time": 41 },
+				{ "time": 42 } ];
+
 let linearRunningPlot = new LinearRunningPlot("linearRunningPlot",
 											  240,
-											  [ { "time": 31 },
-											  	{ "time": 33 },
-											  	{ "time": 36 },
-											  	{ "time": 38 },
-											  	{ "time": 41 },
-											  	{ "time": 42 } ],
+											  efforts,
 											  3);
+
+let resultsTable = new ResultsTable("resultsTable", efforts);
