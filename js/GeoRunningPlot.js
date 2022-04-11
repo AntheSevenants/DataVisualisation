8870d12326f5866d3892bb6ed20b4eb8585aff78
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-04-11 11:03:10 +0000

    Work on GeoRunningPlot

--- js/GeoRunningPlot.js
+++ js/GeoRunningPlot.js
@@ -6,36 +6,87 @@ class GeoRunningPlot extends Map {
 		// To find the focus for our map, we take the middle element of the polyline
 		let centreCoord = polyCoords[Math.round((polyCoords.length - 1) / 3)];
 
-		super(targetElementName, centreCoord, polyCoords=polyCoords);
+		let containerName = `${targetElementName}-container`;
+		let targetElement = d3.select(`#${targetElementName}`);
+		let container = targetElement.append("div")
+					    			.attr("id", containerName);
+
+		super(containerName, centreCoord, polyCoords=polyCoords);
+
+		this.targetElement = targetElement;
 
 		this.segment = segment;
 		this.efforts = efforts;
 
 		this.animatedMarkers = [];
+		this._playing = false;
 
+		this.initPlot();
+		this.initToolbar();
 		this.initAnimation();
 
 		this.ended = false;
+	}
+
+	initPlot() {
+		this.toolbar = this.targetElement.append("div")
+										 .attr("class", "toolbar");
+	}
+
+	initToolbar() {
+		this.toolbar = new Toolbar(this.toolbar);
+
+		this.toolbar.registerButton("play",
+									Constants.playIcon,
+									"",
+									() => { this.togglePlayPause(); });
 	}
 
 	initAnimation() {
 		// Remove existing markers
 		this.animatedMarkers.forEach(animatedMarker => this.map.removeLayer(animatedMarker));
 
-		this.animatedMarkers = this.efforts.map(effort => L.animatedMarker(this.polyline.getLatLngs(),
+		this.animatedMarkers = this.efforts.map((effort, index) => L.animatedMarker(this.polyline.getLatLngs(),
 											  			  { autoStart: false,
 											  			    distance: this.segment["distance"],
+											  			    icon: L.icon({ iconUrl: `minimap/${index}.png`,
+											  			    			   iconSize: 64 }),
 											  			    interval: effort["time"] * 1000,
-											  			    onEnd: () => { this.ended = true; } }));
+											  			    onEnd: () => { this.ended = true;
+											  			    			   if (index == this.efforts.length - 1) {
+											  			    			   	this.playing = false;
+											  			    			   } } }));
 
 		this.animatedMarkers.forEach(animatedMarker => animatedMarker.addTo(this.map));
 	}
 
-	togglePlayPause() {
-		if (this.ended) {
-			this.initAnimation();
-		} 
+	get playing() {
+		return this._playing;
+	}
 
+	set playing(isPlaying) {
+		this._playing = isPlaying;
+
+		if (this.playing) {
+			if (this.ended) {
+				this.initAnimation();
+			}
+
+			this.animate();
+		} else {
+			this.stop();
+		}
+
+		this.toolbar.buttons["play"].text(this.playing ?
+										  Constants.pauseIcon :
+										  Constants.playIcon);
+	}
+
+	togglePlayPause() {
+		this.playing = !this.playing;
+	}
+
+	animate() {
 		this.animatedMarkers.forEach(animatedMarker => animatedMarker.start());
 	}
 
