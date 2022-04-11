7235c2b1f106e2b05aeebd9d5b1cf485d83d7c1f
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-04-11 15:54:58 +0000

    Add segment ids

--- js/Playground.js
+++ js/Playground.js
@@ -5,4 +5,17 @@ let efforts = [ { "time": 31 },
 				{ "time": 41 },
 				{ "time": 42 } ];
 
-let segmentLoader = new SegmentLoader("plots", [ "11092875" ]);
+let segments = [ "10804677",
+				 "11092875",
+				 "13512598",
+				 "14025619",
+				 "22591068",
+				 "2294376",
+				 "23778272",
+				 "26745458",
+				 "8435453",
+				 "8565430",
+				 "8687556",
+				 "900905" ]
+
+let segmentLoader = new SegmentLoader("plots", segments);
