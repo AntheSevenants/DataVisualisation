d190ee923c27fe8c80c741d6e671bd1dd1a97c64
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-04-13 14:37:25 +0000

    Implement constant affirmative action

---
+++ js/ConstantAffirmativeAction.js
@@ -0,0 +1,10 @@
+class ConstantAffirmativeAction extends AffirmativeAction {
+	constructor(range, defaultValue, callback) {
+		super("constant", range, defaultValue, callback);
+
+		this.valueFunction = (value) => value - this.variable;
+		this.formatFunction = () => { let date = Helpers.secondsToDate(this.variable);
+									  let formatted = d3.timeFormat("%M:%S")(date);
+									  return `-${formatted}`; };
+	}
+}
