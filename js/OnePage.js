1676c686d7ceecff245a9dbd582e7880a4c59a94
Anthe Sevenants <anthe.sevenants@student.kuleuven.be>
2022-05-03 08:03:10 +0000

    Add map to one page

--- js/OnePage.js
+++ js/OnePage.js
@@ -1,6 +1,18 @@
-d3.csv("data/strava-leaderboard-segment_finse_piste_heverlee.csv").then((data) => {
+files = [ d3.csv("data/strava-leaderboard-segment_finse_piste_heverlee.csv"),
+		  d3.json("data/segment_900905.json") ];
+
+Promise.all(files).then((promiseData) => {
+	let data = promiseData[0];
+	let segment = promiseData[1];
+
 	new Histogram("chartOriginalHistogram",
 				  data,
 				  new DummyAffirmativeAction(),
 				  true);
+
+	new GeoRunningPlot("chartGeoRunningOriginal",
+					   segment,
+					   data.slice(0, 10),
+					   false,
+					   1);
 });
